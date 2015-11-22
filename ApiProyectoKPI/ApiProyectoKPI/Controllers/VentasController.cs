8dbcfde9be53b076b9fc18cdccf338e05267e231
marJorieNav <marjorieanavarro@gmail.com>
2015-11-22 17:40:14 -0600

    registrar venta api

---
+++ ApiProyectoKPI/ApiProyectoKPI/Controllers/VentasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiProyectoKPI.Controllers.DataBaseContext;
+using ApiProyectoKPI.Models;
+
+namespace ApiProyectoKPI.Controllers
+{
+    public class VentasController : ApiController
+    {
+        private ApiKPIsContext db = new ApiKPIsContext();
+
+        // GET: api/Ventas
+        public IQueryable<Venta> Getventas()
+        {
+            return db.Ventas;
+        }
+
+        // GET: api/Ventas/5
+        [ResponseType(typeof(Venta))]
+        public IHttpActionResult GetVenta(int id)
+        {
+            Venta venta = db.Ventas.Find(id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(venta);
+        }
+
+        // PUT: api/Ventas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutVenta(int id, Venta venta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != venta.VentaID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(venta).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VentaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Ventas
+        [ResponseType(typeof(Venta))]
+        public IHttpActionResult PostVenta(Venta venta)
+        {
+
+            var Prospecto = db.Prospectoes.Where(b => b.Identificacion == venta.Prospecto.Identificacion).Include(b => b.Usuario).FirstOrDefault();
+            venta.Prospecto = Prospecto;
+            venta.Usuario = Prospecto.Usuario;
+            //venta.Prospecto = db.Prospectoes.Find(venta.Prospecto);
+            //venta.Usuario = db.Prospectoes.Find(venta.Prospecto.Identificacion).(include =>Usuario).FirstOrDefault();
+
+
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Ventas.Add(venta);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = venta.VentaID }, venta);
+        }
+
+        // DELETE: api/Ventas/5
+        [ResponseType(typeof(Venta))]
+        public IHttpActionResult DeleteVenta(int id)
+        {
+            Venta venta = db.Ventas.Find(id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            db.Ventas.Remove(venta);
+            db.SaveChanges();
+
+            return Ok(venta);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool VentaExists(int id)
+        {
+            return db.Ventas.Count(e => e.VentaID == id) > 0;
+        }
+    }
+}
